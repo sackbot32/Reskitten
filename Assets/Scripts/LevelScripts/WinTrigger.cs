8da8aa717247012a45461c60b48a269a441691ac
sackbot32 <82033635+sackbot32@users.noreply.github.com>
2025-02-17 14:20:35 +0100

    WonLevelSaved

--- Assets/Scripts/LevelScripts/WinTrigger.cs
+++ Assets/Scripts/LevelScripts/WinTrigger.cs
@@ -3,12 +3,17 @@ using UnityEngine;
 public class WinTrigger : MonoBehaviour
 {
     private bool hasWon;
+    public int levelIndex;
     private void OnTriggerStay(Collider other)
     {
         if(other.tag == "Player")
         {
             if (!ToolUser.instance.canChange && !hasWon)
             {
+                if(PlayerPrefs.GetInt(LevelSelectHud.LEVELKEY) < levelIndex)
+                {
+                    PlayerPrefs.SetInt(LevelSelectHud.LEVELKEY,levelIndex);
+                }
                 hasWon = true;
                 PauseHud.instance.WinScreen();
             }
--- Assets/Scripts/UI/LevelSelectHud.cs
+++ Assets/Scripts/UI/LevelSelectHud.cs
@@ -1,14 +1,43 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+[Serializable]
+public class LevelButtonObj
+{
+    public int levelPos;
+    public Button levelButton;
+    public Image catSiluete;
+    public Image catImage;
+
+    public void CheckForLevel()
+    {
+        if (PlayerPrefs.GetInt(LevelSelectHud.LEVELKEY) + 1 < levelPos)
+        {
+            levelButton.interactable = false;
+            catImage.color = new Color(1,1,1,0);
+            catSiluete.color = new Color(1,1,1,0.5f);
+
+        }
+    }
+}
 
 public class LevelSelectHud : MonoBehaviour
 {
+    public static string LEVELKEY = "LastBeaten";
     public InputActionReference pauseInput;
+    public List<LevelButtonObj> levelButtons = new List<LevelButtonObj>();
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
-
+        foreach (LevelButtonObj lvlButton in levelButtons)
+        {
+            lvlButton.CheckForLevel();
+        }
     }
 
     // Update is called once per frame
